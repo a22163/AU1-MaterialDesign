892c90f973f740682581f0fd0598f6eda59695e1
Alberto Herandez <albertohdez@gmail.com>
2020-09-28 17:51:36 +0200

    1º commit

---
+++ app/src/main/java/com/svalero/a1_5_0_3_relativelayout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.svalero.a1_5_0_3_relativelayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
